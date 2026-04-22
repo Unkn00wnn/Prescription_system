8ee3dc9eed689e1c65ef7e924c2da26c427656b6
Kushal Mehta <115134674+Unkn00wnn@users.noreply.github.com>
2026-04-22 22:34:00 +0530

    Delete inventory directory

--- inventory/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path('', views.medicine_list, name='medicine_list'),
-    path('add/', views.medicine_create, name='medicine_create'),
-    path('<int:pk>/', views.medicine_detail, name='medicine_detail'),
-]
