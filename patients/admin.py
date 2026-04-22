d34674352790730260782775320fca7402672c9a
Kushal Mehta <115134674+Unkn00wnn@users.noreply.github.com>
2026-04-22 22:33:18 +0530

    Delete patients/admin.py

--- patients/admin.py
+++
@@ -1,8 +0,0 @@
-from django.contrib import admin
-from .models import Patient
-
-@admin.register(Patient)
-class PatientAdmin(admin.ModelAdmin):
-    list_display = ('id', 'name', 'age', 'gender', 'contact_number', 'status', 'created_at')
-    list_filter = ('status', 'gender')
-    search_fields = ('name', 'contact_number')
